1207c293f0a75f1c85ac6c10e8ec2b15151273c7
leminhtanvatc <leminhtanvatc@outlook.com>
2026-05-17 21:55:27 +0700

    fix No module named 'models'

--- posterv2/main_8.py
+++ posterv2/main_8.py
@@ -20,7 +20,7 @@ import numpy as np
 import datetime
 from torchsampler import ImbalancedDatasetSampler
 
-from models.PosterV2_8cls import *
+from .models.PosterV2_8cls import *
 
 now = datetime.datetime.now()
 time_str = now.strftime("[%m-%d]-[%H-%M]-")
